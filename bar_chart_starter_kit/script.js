0a6ffaa51d913a852caf60831a5109969dbe4623
Ester <espi0001@stud.kea.dk>
2024-10-03 14:55:55 +0200

    bv

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,5 +1,18 @@
 const list = document.querySelector("ul");
+const valueArr = [];
 
-const li = document.createElement("li");
-li.style.setProperty("--height", "30");
-list.appendChild(li);
+setInterval(generateBars, 1000);
+
+function generateBars() {
+  const randoNum = Math.floor(Math.random() * 100);
+  if (valueArr.length > 19) {
+    valueArr.shift();
+    list.removeChild(list.firstChild);
+  }
+  valueArr.push(randoNum);
+
+  console.log(valueArr);
+  const li = document.createElement("li");
+  li.style.setProperty("--height", randoNum);
+  list.appendChild(li);
+}
