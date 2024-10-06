55bfb827e590ade27f2a7c6646e62946d0384bd8
Ester <espi0001@stud.kea.dk>
2024-10-06 19:00:45 +0200

    √

--- real-capitalization/capitalization.js
+++ real-capitalization/capitalization.js
@@ -1,10 +1,10 @@
-// Laver en konstant variabel med variablen aName og værdien "Ester", som er en streng som der kan manipuleres
-const aName = "Ester";
+// Laver en konstant variabel med variablen aName og værdien "Peter", som er en streng som der kan manipuleres
+const aName = "Peter";
 
 // Gør det første bogstav stort og resten små
 const capitalizedName = aName[0].toUpperCase() + aName.substring(1).toLowerCase();
 
-console.log(capitalizedName); // Logger 'Ester'
+console.log(capitalizedName); // Logger 'Peter'
 
 // 	aName[0].toUpperCase(): Tager det første tegn i strengen (indeks 0, altså "P") og konverterer det til stort.
 // aName.substring(1).toLowerCase(): Tager resten af strengen fra indeks 1 og frem (det er "eter") og konverterer det til små bogstaver.
